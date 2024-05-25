6f54624c88f52bab4b55c90f36a47cd0811c52ab
Divyanshu6797 <divyanshu6797@gmail.com>
2024-05-25 10:08:46 +0530

    templates and errors

--- djangoproj/views.py
+++ djangoproj/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Hello, Django!')
+    # return HttpResponse('Hello, Django!')
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse('About us')
 
 def contact(request):
-    return HttpResponse('Contact us')
+    # return HttpResponse('Contact us')
+    return render(request, 'websites/contact.html')
 
